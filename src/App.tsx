86db5b51a83839d0b268356d7c007f5354485963
Hostyn <ruben@martinezhostyn.com>
2023-02-25 13:54:59 +0100

    Add routes and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,15 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = (): JSX.Element => {
-  const [count, setCount] = useState(0)
-
-  const handleClick = async (): Promise<void> => {
-    await fetch((import.meta.env.VITE_BACKEND_URI as string) + '/registry', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: 'test@test.com',
-        password: 'passsword',
-      }),
-    })
-
-    const res = await fetch(
-      (import.meta.env.VITE_BACKEND_URI as string) + '/measurements',
-      {
-        credentials: 'include',
-        method: 'GET',
-      }
-    )
-
-    console.log(await res.json())
-  }
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count => count + 1)
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={handleClick}>Test</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registry" element={<Register />} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
